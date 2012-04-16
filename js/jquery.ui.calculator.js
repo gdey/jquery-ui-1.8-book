8c0b405057391e0eabd1b6b619d348a8d6da453d
Gautam Dey <gautam.dey77@gmail.com>
2012-04-15 22:58:06 -0700

    Initial file for calculator widget

---
+++ js/jquery.ui.calculator.js
@@ -0,0 +1,20 @@
+(function($){
+
+// Namespacing out our calculator plugin for jQuery.
+$.widget("ui.calculator",{
+
+   options: {
+       autoShow: true,
+       currentSum: []
+   },
+   _create: function(){
+   },
+   destroy: function(){
+   },
+   disable: function(){
+   },
+   enable: function(){
+   }
+);
+
+}(jQuery));
